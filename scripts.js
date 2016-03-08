556bddef8109a09cc5211a38d476ca39228e6554
Keith Moore <keith.m4@gmail.com>
2016-03-08 14:45:57 -0500

    Added gameOver function
    game-header tells you who won the game and highlights winning squares.

--- scripts.js
+++ scripts.js
@@ -10,6 +10,8 @@ var winners = [
 ];
 
 var whosTurn = 1;
+var playerOneMarkings = [];
+var playerTwoMarkings = [];
 
 function addSymbol(element){
 	var gameHeader = document.getElementById('game-header')
@@ -19,31 +21,67 @@ function addSymbol(element){
 			//It's X's turn. So, we have an empty square, and it's X's turn. Put an X in.
 			element.innerHTML = 'X';
 			whosTurn = 2;
-			gameHeader.innerHTML = "It is Player 2's turn";
+			gameHeader.innerHTML = "player 2's turn";
 			gameHeader.className = 'player-two';
 			//Get rid of class 'empty', and add who took the square
-			console.log(element.classList);
 			element.classList.remove('empty');
 			element.classList.add('playerOneHasThisSpace');
+			playerOneMarkings.push(element.id);
 		}else{
 			//It has to be O's turn. Put an O in.
 			element.innerHTML = 'O';
 			whosTurn = 1;
-			gameHeader.innerHTML = "It is Player 1's turn";
+			gameHeader.innerHTML = "player 1's turn";
 			gameHeader.className = 'player-one';
 			element.classList.remove('empty');
 			element.classList.add('playerTwoHasThisSpace');
+			playerTwoMarkings.push(element.id);
 		}
 	}else{
-		gameHeader.innerHTML = "This box is taken";
+		gameHeader.innerHTML = "This box is taken.";
 		gameHeader.className = 'red';
+	}
+	checkWin();
+}
+
+function checkWin() {
+	// if rowCount gets to 3, then we have a winner
+	var playerOneRowCount = 0;
+	var playerTwoRowCount = 0;
+	var thisWinCombination;
+	// loop through all winning combinations
+	for (i=0; i<winners.length; i++) {
+		playerOneRowCount = 0;
+		playerTwoRowCount = 0;
+		thisWinCombination = winners[i];
+		// check if all elements in the winners array exist in the current player
+		for (j=0; j<thisWinCombination.length; j++) {
+			if(playerOneMarkings.indexOf(thisWinCombination[j]) > -1) {
+				playerOneRowCount++; // if the the index is not -1, then it's in there
+			} else if (playerTwoMarkings.indexOf(thisWinCombination[j]) > -1) {
+				playerTwoRowCount++;
+			}
+		}
+		if (playerOneRowCount === 3 || playerTwoRowCount === 3)  {
+			gameOver(thisWinCombination);
+		}
 	}
 }
 
+function gameOver(combo) {
+	var gameHeader = document.getElementById('game-header');
+	for (i=0;i<combo.length;i++) {
+		document.getElementById(combo[i]).classList.add('winner');
+		if (combo[i].indexOf('a') > -1) {
+			gameHeader.innerHTML = "Player 1 won the game!";
+		} else if (combo[i].indexOf('b') > -1) {
+			gameHeader.innerHTML = "Player 2 won the game!";
+		}
+	}
+}
 
 var squareWidth = document.getElementById('a1').clientWidth;
 var squares = document.getElementsByClassName('square');
 for(i=0; i<squares.length; i++){
 	squares[i].style.height = squareWidth + 'px';
 }
-
